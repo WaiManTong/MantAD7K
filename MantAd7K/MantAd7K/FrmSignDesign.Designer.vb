53fdd933d3e0e383d9586be10d4f7ac89d665891
Wai Man Tong Ramirez <waiman.tongr@gmail.com>
2023-12-12 00:25:56 -0500

    Sign

--- MantAd7K/MantAd7K/FrmSignDesign.Designer.vb
+++ MantAd7K/MantAd7K/FrmSignDesign.Designer.vb
@@ -28,37 +28,40 @@ Partial Class FrmSignDesign
         Me.TextBox2 = New System.Windows.Forms.TextBox()
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
-        Me.BtnAdd = New System.Windows.Forms.Button()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
-        Me.BtnClear = New System.Windows.Forms.Button()
-        Me.BtnPreview = New System.Windows.Forms.Button()
+        Me.BtnLoadTemplate = New System.Windows.Forms.Button()
+        Me.LbxFields = New System.Windows.Forms.ListBox()
+        Me.BtnClearConf = New System.Windows.Forms.Button()
         Me.BtnSave = New System.Windows.Forms.Button()
         Me.BtnTemplate = New System.Windows.Forms.Button()
         Me.BtnClose = New System.Windows.Forms.Button()
+        Me.LbxADFields = New System.Windows.Forms.ListBox()
+        Me.LbxCSVTemplate = New System.Windows.Forms.ListBox()
         Me.SuspendLayout()
         '
         'RtxtDesign
         '
-        Me.RtxtDesign.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
-            Or System.Windows.Forms.AnchorStyles.Left) _
+        Me.RtxtDesign.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.RtxtDesign.BackColor = System.Drawing.SystemColors.Desktop
         Me.RtxtDesign.ForeColor = System.Drawing.Color.LimeGreen
-        Me.RtxtDesign.Location = New System.Drawing.Point(12, 318)
+        Me.RtxtDesign.Location = New System.Drawing.Point(8, 219)
+        Me.RtxtDesign.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.RtxtDesign.Name = "RtxtDesign"
         Me.RtxtDesign.ReadOnly = True
-        Me.RtxtDesign.Size = New System.Drawing.Size(732, 326)
+        Me.RtxtDesign.Size = New System.Drawing.Size(450, 106)
         Me.RtxtDesign.TabIndex = 0
         Me.RtxtDesign.Text = ""
         '
         'WbSign
         '
-        Me.WbSign.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+        Me.WbSign.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
             Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.WbSign.Location = New System.Drawing.Point(12, 12)
-        Me.WbSign.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WbSign.Location = New System.Drawing.Point(8, 8)
+        Me.WbSign.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.WbSign.MinimumSize = New System.Drawing.Size(13, 13)
         Me.WbSign.Name = "WbSign"
-        Me.WbSign.Size = New System.Drawing.Size(1062, 300)
+        Me.WbSign.Size = New System.Drawing.Size(515, 202)
         Me.WbSign.TabIndex = 1
         '
         'TextBox1
@@ -66,9 +69,10 @@ Partial Class FrmSignDesign
         Me.TextBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.TextBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark
         Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.TextBox1.Location = New System.Drawing.Point(750, 344)
+        Me.TextBox1.Location = New System.Drawing.Point(11, 347)
+        Me.TextBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(160, 19)
+        Me.TextBox1.Size = New System.Drawing.Size(107, 13)
         Me.TextBox1.TabIndex = 2
         '
         'TextBox2
@@ -76,9 +80,10 @@ Partial Class FrmSignDesign
         Me.TextBox2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.TextBox2.BackColor = System.Drawing.SystemColors.ControlDarkDark
         Me.TextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.TextBox2.Location = New System.Drawing.Point(916, 344)
+        Me.TextBox2.Location = New System.Drawing.Point(122, 347)
+        Me.TextBox2.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(158, 19)
+        Me.TextBox2.Size = New System.Drawing.Size(108, 13)
         Me.TextBox2.TabIndex = 3
         '
         'Label1
@@ -86,9 +91,10 @@ Partial Class FrmSignDesign
         Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label1.AutoSize = True
         Me.Label1.ForeColor = System.Drawing.SystemColors.Info
-        Me.Label1.Location = New System.Drawing.Point(750, 321)
+        Me.Label1.Location = New System.Drawing.Point(11, 332)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(60, 20)
+        Me.Label1.Size = New System.Drawing.Size(40, 13)
         Me.Label1.TabIndex = 4
         Me.Label1.Text = "Campo"
         '
@@ -97,68 +103,60 @@ Partial Class FrmSignDesign
         Me.Label2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.Label2.AutoSize = True
         Me.Label2.ForeColor = System.Drawing.SystemColors.Info
-        Me.Label2.Location = New System.Drawing.Point(916, 321)
+        Me.Label2.Location = New System.Drawing.Point(122, 332)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(2, 0, 2, 0)
         Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(46, 20)
+        Me.Label2.Size = New System.Drawing.Size(31, 13)
         Me.Label2.TabIndex = 5
         Me.Label2.Text = "Valor"
         '
-        'BtnAdd
+        'BtnLoadTemplate
         '
-        Me.BtnAdd.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.BtnAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.BtnAdd.ForeColor = System.Drawing.SystemColors.Info
-        Me.BtnAdd.Location = New System.Drawing.Point(982, 384)
-        Me.BtnAdd.Name = "BtnAdd"
-        Me.BtnAdd.Size = New System.Drawing.Size(92, 33)
-        Me.BtnAdd.TabIndex = 6
-        Me.BtnAdd.Text = "&Agregar"
-        Me.BtnAdd.UseVisualStyleBackColor = True
+        Me.BtnLoadTemplate.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnLoadTemplate.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnLoadTemplate.ForeColor = System.Drawing.SystemColors.Info
+        Me.BtnLoadTemplate.Location = New System.Drawing.Point(462, 347)
+        Me.BtnLoadTemplate.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.BtnLoadTemplate.Name = "BtnLoadTemplate"
+        Me.BtnLoadTemplate.Size = New System.Drawing.Size(61, 21)
+        Me.BtnLoadTemplate.TabIndex = 6
+        Me.BtnLoadTemplate.Text = "&Agregar"
+        Me.BtnLoadTemplate.UseVisualStyleBackColor = True
         '
-        'ListBox1
+        'LbxFields
         '
-        Me.ListBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.ListBox1.BackColor = System.Drawing.SystemColors.ControlDarkDark
-        Me.ListBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 20
-        Me.ListBox1.Location = New System.Drawing.Point(750, 384)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(223, 260)
-        Me.ListBox1.TabIndex = 7
+        Me.LbxFields.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LbxFields.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.LbxFields.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.LbxFields.FormattingEnabled = True
+        Me.LbxFields.Location = New System.Drawing.Point(10, 368)
+        Me.LbxFields.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.LbxFields.Name = "LbxFields"
+        Me.LbxFields.Size = New System.Drawing.Size(108, 104)
+        Me.LbxFields.TabIndex = 7
         '
-        'BtnClear
+        'BtnClearConf
         '
-        Me.BtnClear.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.BtnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.BtnClear.ForeColor = System.Drawing.SystemColors.Info
-        Me.BtnClear.Location = New System.Drawing.Point(982, 423)
-        Me.BtnClear.Name = "BtnClear"
-        Me.BtnClear.Size = New System.Drawing.Size(92, 33)
-        Me.BtnClear.TabIndex = 8
-        Me.BtnClear.Text = "&Limpiar"
-        Me.BtnClear.UseVisualStyleBackColor = True
-        '
-        'BtnPreview
-        '
-        Me.BtnPreview.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-        Me.BtnPreview.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.BtnPreview.ForeColor = System.Drawing.SystemColors.Info
-        Me.BtnPreview.Location = New System.Drawing.Point(982, 462)
-        Me.BtnPreview.Name = "BtnPreview"
-        Me.BtnPreview.Size = New System.Drawing.Size(92, 33)
-        Me.BtnPreview.TabIndex = 9
-        Me.BtnPreview.Text = "&Visualizar"
-        Me.BtnPreview.UseVisualStyleBackColor = True
+        Me.BtnClearConf.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.BtnClearConf.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnClearConf.ForeColor = System.Drawing.SystemColors.Info
+        Me.BtnClearConf.Location = New System.Drawing.Point(462, 376)
+        Me.BtnClearConf.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.BtnClearConf.Name = "BtnClearConf"
+        Me.BtnClearConf.Size = New System.Drawing.Size(61, 21)
+        Me.BtnClearConf.TabIndex = 8
+        Me.BtnClearConf.Text = "&Limpiar"
+        Me.BtnClearConf.UseVisualStyleBackColor = True
         '
         'BtnSave
         '
         Me.BtnSave.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.BtnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.BtnSave.ForeColor = System.Drawing.SystemColors.Info
-        Me.BtnSave.Location = New System.Drawing.Point(982, 501)
+        Me.BtnSave.Location = New System.Drawing.Point(462, 401)
+        Me.BtnSave.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.BtnSave.Name = "BtnSave"
-        Me.BtnSave.Size = New System.Drawing.Size(92, 33)
+        Me.BtnSave.Size = New System.Drawing.Size(61, 21)
         Me.BtnSave.TabIndex = 10
         Me.BtnSave.Text = "&Guardar"
         Me.BtnSave.UseVisualStyleBackColor = True
@@ -168,9 +166,10 @@ Partial Class FrmSignDesign
         Me.BtnTemplate.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.BtnTemplate.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.BtnTemplate.ForeColor = System.Drawing.SystemColors.Info
-        Me.BtnTemplate.Location = New System.Drawing.Point(982, 540)
+        Me.BtnTemplate.Location = New System.Drawing.Point(462, 426)
+        Me.BtnTemplate.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.BtnTemplate.Name = "BtnTemplate"
-        Me.BtnTemplate.Size = New System.Drawing.Size(92, 33)
+        Me.BtnTemplate.Size = New System.Drawing.Size(61, 21)
         Me.BtnTemplate.TabIndex = 11
         Me.BtnTemplate.Text = "&Plantilla"
         Me.BtnTemplate.UseVisualStyleBackColor = True
@@ -180,32 +179,59 @@ Partial Class FrmSignDesign
         Me.BtnClose.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
         Me.BtnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.BtnClose.ForeColor = System.Drawing.SystemColors.Info
-        Me.BtnClose.Location = New System.Drawing.Point(982, 579)
+        Me.BtnClose.Location = New System.Drawing.Point(462, 454)
+        Me.BtnClose.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.BtnClose.Name = "BtnClose"
-        Me.BtnClose.Size = New System.Drawing.Size(92, 33)
+        Me.BtnClose.Size = New System.Drawing.Size(61, 21)
         Me.BtnClose.TabIndex = 12
         Me.BtnClose.Text = "&Cerrar"
         Me.BtnClose.UseVisualStyleBackColor = True
         '
+        'LbxADFields
+        '
+        Me.LbxADFields.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LbxADFields.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.LbxADFields.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.LbxADFields.FormattingEnabled = True
+        Me.LbxADFields.Location = New System.Drawing.Point(122, 368)
+        Me.LbxADFields.Margin = New System.Windows.Forms.Padding(2)
+        Me.LbxADFields.Name = "LbxADFields"
+        Me.LbxADFields.Size = New System.Drawing.Size(108, 104)
+        Me.LbxADFields.TabIndex = 13
+        '
+        'LbxCSVTemplate
+        '
+        Me.LbxCSVTemplate.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.LbxCSVTemplate.BackColor = System.Drawing.SystemColors.ControlDarkDark
+        Me.LbxCSVTemplate.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.LbxCSVTemplate.FormattingEnabled = True
+        Me.LbxCSVTemplate.Location = New System.Drawing.Point(234, 368)
+        Me.LbxCSVTemplate.Margin = New System.Windows.Forms.Padding(2)
+        Me.LbxCSVTemplate.Name = "LbxCSVTemplate"
+        Me.LbxCSVTemplate.Size = New System.Drawing.Size(224, 104)
+        Me.LbxCSVTemplate.TabIndex = 14
+        '
         'FrmSignDesign
         '
-        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.SystemColors.Desktop
-        Me.ClientSize = New System.Drawing.Size(1086, 656)
+        Me.ClientSize = New System.Drawing.Size(531, 487)
+        Me.Controls.Add(Me.LbxCSVTemplate)
+        Me.Controls.Add(Me.LbxADFields)
         Me.Controls.Add(Me.BtnClose)
         Me.Controls.Add(Me.BtnTemplate)
         Me.Controls.Add(Me.BtnSave)
-        Me.Controls.Add(Me.BtnPreview)
-        Me.Controls.Add(Me.BtnClear)
-        Me.Controls.Add(Me.ListBox1)
-        Me.Controls.Add(Me.BtnAdd)
+        Me.Controls.Add(Me.BtnClearConf)
+        Me.Controls.Add(Me.LbxFields)
+        Me.Controls.Add(Me.BtnLoadTemplate)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.WbSign)
         Me.Controls.Add(Me.RtxtDesign)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
+        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
         Me.Name = "FrmSignDesign"
         Me.Text = " "
         Me.ResumeLayout(False)
@@ -219,11 +245,12 @@ Partial Class FrmSignDesign
     Friend WithEvents TextBox2 As Windows.Forms.TextBox
     Friend WithEvents Label1 As Windows.Forms.Label
     Friend WithEvents Label2 As Windows.Forms.Label
-    Friend WithEvents BtnAdd As Windows.Forms.Button
-    Friend WithEvents ListBox1 As Windows.Forms.ListBox
-    Friend WithEvents BtnClear As Windows.Forms.Button
-    Friend WithEvents BtnPreview As Windows.Forms.Button
+    Friend WithEvents BtnLoadTemplate As Windows.Forms.Button
+    Friend WithEvents LbxFields As Windows.Forms.ListBox
+    Friend WithEvents BtnClearConf As Windows.Forms.Button
     Friend WithEvents BtnSave As Windows.Forms.Button
     Friend WithEvents BtnTemplate As Windows.Forms.Button
     Friend WithEvents BtnClose As Windows.Forms.Button
+    Friend WithEvents LbxADFields As Windows.Forms.ListBox
+    Friend WithEvents LbxCSVTemplate As Windows.Forms.ListBox
 End Class
--- MantAd7K/MantAd7K/FrmSignDesign.vb
+++ MantAd7K/MantAd7K/FrmSignDesign.vb
@@ -1,11 +1,58 @@
-﻿Imports System.IO
+﻿Imports System.Collections.Generic
+Imports System.Globalization
+Imports System.IO
+Imports System.Linq
+Imports System.Text.RegularExpressions
 Imports System.Windows.Forms
+Imports CsvHelper
+Imports CsvHelper.Configuration
 
 Public Class FrmSignDesign
+
+    Private Sub BuscarCadenasEntreCorchetes()
+        ' Obtén el contenido del RichTextBox
+        Dim contenido As String = RtxtDesign.Text
+        ' Crea un HashSet para almacenar cadenas únicas
+        Dim cadenasUnicas As New HashSet(Of String)
+
+        ' Encuentra todas las cadenas encerradas entre {} dentro del contenido del <body>
+        Dim regex As New Regex("<body[^>]*>([\s\S]*?)<\/body>", RegexOptions.IgnoreCase)
+        Dim coincidencias As MatchCollection = regex.Matches(contenido)
+
+        ' Muestra las cadenas encontradas
+        For Each coincidencia As Match In coincidencias
+            Dim contenidoBody As String = coincidencia.Groups(1).Value
+            Dim regexCadenas As New Regex("\{([^{}]+)\}")
+            Dim coincidenciasCadenas As MatchCollection = regexCadenas.Matches(contenidoBody)
+
+            ' Muestra las cadenas encontradas dentro del <body>
+            For Each cadena As Match In coincidenciasCadenas
+                cadenasUnicas.Add(cadena.Groups(1).Value)
+            Next
+        Next
+        LbxFields.Items.Clear()
+        LbxFields.Items.AddRange(cadenasUnicas.ToArray())
+
+    End Sub
+
     Private Sub FrmSignDesign_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Asigna el código HTML de ejemplo al RichTextBox
         RtxtDesign.Text = "<html><body><h1>Hola, mundo!</h1></body></html>"
-        ActualizarVistaPrevia()
+        '--------------------------
+        ' Lee la primera línea del archivo CSV para obtener las cabeceras
+        Dim primeraLinea As String = File.ReadLines("C:\Users\cgrimaldi\source\repos\MantAD7K\MantAd7K\MantAd7K\bin\Debug\data\pe.doerun.local\UsuariosAD.csv").FirstOrDefault()
+
+        ' Verifica si hay una primera línea
+        If Not String.IsNullOrEmpty(primeraLinea) Then
+            ' Dividir la línea en campos utilizando coma como delimitador
+            Dim cabeceras As String() = primeraLinea.Split(","c)
+
+            ' Limpia el ListBox y agrega las cabeceras
+            LbxADFields.Items.Clear()
+            LbxADFields.Items.AddRange(cabeceras)
+        Else
+            MessageBox.Show("El archivo CSV no contiene datos.", "Advertencia", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If
 
     End Sub
 
@@ -40,5 +87,9 @@ Public Class FrmSignDesign
             ' Asigna el contenido al RichTextBox
             RtxtDesign.Text = archivoHTML
         End If
+
+        BuscarCadenasEntreCorchetes()
+
     End Sub
+
 End Class
